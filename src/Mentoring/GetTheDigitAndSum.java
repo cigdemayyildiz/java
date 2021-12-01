ab68105b37582fe155eaeb9701bc6ed2d6729ecc
Cigdem <cigdemrespect@gmail.com>
2021-12-01 10:58:12 -0600

    Get The Digit and Sum

--- src/Mentoring/GetTheDigitAndSum.java
+++ src/Mentoring/GetTheDigitAndSum.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 public class GetTheDigitAndSum {
 
     public static void main(String[] args) {
-
         /*
         1. Write a program that will accept number
         numbers like 578432 and find  multiplication
