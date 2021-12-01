1c937269081fdba503cd133e386bc06cc1f6eba7
Cigdem <cigdemrespect@gmail.com>
2021-12-01 10:54:24 -0600

    Static and Instance Blocks

--- src/Mentoring/JavaRecap/A12StaticAndInstanceBlock.java
+++ src/Mentoring/JavaRecap/A12StaticAndInstanceBlock.java
@@ -7,7 +7,6 @@ public class A12StaticAndInstanceBlock {
     // The main difference between them is all about calling static block which belongs to class 1 time and all the way top.
     // Instance block belongs object and it will go after static as many as object you created.
 
-
     static {
         System.out.println("Static block"); // main method icine alamazsin bu sinifa ait methoda ait degil
     }
@@ -27,7 +26,5 @@ public class A12StaticAndInstanceBlock {
         {
             System.out.println("Instance block");
         }
-
-
     }
 }
